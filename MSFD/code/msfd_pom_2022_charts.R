9336417502cfc04c6a0f81c2a963bfc2e9d9cc31
dosullivan019 <d.osullivan019@gmail.com>
2022-11-11 13:41:41 +0000

    updates to match new file structure

--- MSFD/code/msfd_pom_2022_charts.R
+++ MSFD/code/msfd_pom_2022_charts.R
@@ -17,7 +17,7 @@ library(waffle)
 library(packcircles)
 
 ################### ReportNetData ##################################
-pom_report_net_data = read_excel('ReportNetFile_Oct2022_V13.xlsx')
+pom_report_net_data = read_excel('./ReportNetFiles2022/ReportNetFile_Oct2022_V31.xlsx')
 descriptors_list = c('D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11')
 names(descriptors_list) = c('D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11')
 
@@ -75,7 +75,7 @@ heatmap_data =
   mutate(PolicyNational = strsplit(as.character(PolicyNational), ";")) %>% 
   unnest(PolicyNational) %>%
   select(MeasureCode, GEScomponent, CoordinationLevel, PoliciesConventions, MeasurePurpose, ResponsibleCompetentAuthority,
-         DPSiR, ImplementationStatus, UpdateType, RelevantKTMs, Feature, `Type of measure`, PolicyNational)
+         DAPSIR, ImplementationStatus, UpdateType, RelevantKTMs, Feature, MeasureType, PolicyNational)
 
 
 
@@ -112,14 +112,14 @@ heatmap_data %>%
   theme_minimal() +
   theme(plot.title = element_text(hjust=0.5)) +
   guides(fill="none")
-ggsave('POM_charts/policy_descriptor_balloon.jpg')
+ggsave('POM_charts/1. Policy Bubble Chart_v3.jpg')
 
-#### TREEMAP - DPSiR and Descriptor ####
+#### TREEMAP - DAPSIR and Descriptor ####
 heatmap_data %>%
-  group_by(DPSiR, GEScomponent) %>%
+  group_by(DAPSIR, GEScomponent) %>%
   summarise(n = as.double(n_distinct(MeasureCode))) %>%
   treemap(
-    index = c("GEScomponent", "DPSiR"),
+    index = c("GEScomponent", "DAPSIR"),
     vSize = "n",
     vColor = "GEScomponent",
     type='categorical',
@@ -135,12 +135,12 @@ heatmap_data %>%
                     '#176552', # marine litter
                     '#1A78B6' # underwater noise
     )),
-    title='Number of measures for each Descriptor and DPSiR',
+    title='Number of measures for each Descriptor and DAPSIR',
     position.legend='bottom',
     reverse.legend=TRUE,
     title.legend='Descriptor'
   )
-ggsave('POM_charts/pom_dpsir_descriptor_treemap.jpg')  
+ggsave('POM_charts/pom_DAPSIR_descriptor_treemap.jpg')  
 
 
 #### CIRCULAR - number of descriptors####
@@ -179,31 +179,51 @@ heatmap_data %>%
         plot.title = element_text(hjust=0.5),
         axis.text.x = element_text(face='bold', size=12)) +
   coord_polar()
-ggsave('POM_charts/pom_descriptor_count_circular.jpg')
+ggsave('POM_charts/6. Circle diagram_v3.jpg')
 
 #### LOLLIPOP - Implementation Status ####
-heatmap_data %>%  
-  group_by(ImplementationStatus) %>%
-  summarise(n = n_distinct(MeasureCode)) %>%
-  drop_na() %>%
-  ggdotchart(x = "ImplementationStatus", 
-             y = "n",
-             size=3,
-             sorting = "descending",
-             add = "segments",
-             rotate = TRUE,
-             dot.size = 9,
-             label = "n", 
-             color='#145C9E',
-             font.label = list(color = "white", size = 10, vjust = 0.5, face='bold'),
-             ggtheme = theme_pubr(),
-             title='Implementation Status of Measures',
-             ylab = '# of measures',
-             xlab = ''
-  ) +
+# heatmap_data %>%  
+#   group_by(ImplementationStatus) %>%
+#   summarise(n = n_distinct(MeasureCode)) %>%
+#   drop_na() %>%
+#   ggdotchart(x = "ImplementationStatus", 
+#              y = "n",
+#              size=3,
+#              sorting = "descending",
+#              add = "segments",
+#              rotate = TRUE,
+#              dot.size = 9,
+#              label = "n", 
+#              color='#145C9E',
+#              font.label = list(color = "white", size = 10, vjust = 0.5, face='bold'),
+#              ggtheme = theme_pubr(),
+#              title='Implementation Status of Measures',
+#              ylab = '# of measures',
+#              xlab = ''
+#   ) +
+#   scale_y_continuous(expand=c(0,1), limits=c(0,120)) +
+#   theme(plot.title = element_text(hjust=0.5))
+# ggsave('POM_charts/pom_implementation_status.jpg') 
+
+#### STACKED - Implementation Status and Update Type ####
+pom_report_net_data %>%
+  filter(ImplementationStatus != 'Measure withdrawn') %>%
+  ggplot() +
+  geom_bar(aes(x=UpdateType, fill=ImplementationStatus)) +
+  scale_x_discrete(expand=c(0,0)) +
   scale_y_continuous(expand=c(0,1), limits=c(0,120)) +
-  theme(plot.title = element_text(hjust=0.5))
-ggsave('POM_charts/pom_implementation_status.jpg') 
+  scale_fill_brewer(palette='RdBu') +
+  labs(y='Number of Measures',
+       x='Update Type',
+       fill='') +
+  coord_flip() +
+  theme_minimal() +
+  theme(legend.position = 'top',
+        axis.text = element_text(size=11),
+        axis.title = element_text(size=12),
+        legend.text = element_text(size=10),
+        legend.text.align = 0)
+ggsave('POM_charts/13. Measure Status_v3.jpg')
 
 #### BUBBLE CHART - Measure Update Type ####
 # heatmap_data %>%
@@ -227,48 +247,48 @@ ggsave('POM_charts/pom_implementation_status.jpg')
 #   guides(fill="none",
 #          size="none")
 # ggsave('POM_charts/pom_update_type.jpg')
-data_circle = 
-  heatmap_data %>%
-    group_by(UpdateType) %>%
-    summarise(n = n_distinct(MeasureCode))
-packing = circleProgressiveLayout(data_circle$n, sizetype='area')
-# We can add these packing information to the initial data frame
-data_circle <- cbind(data_circle, packing)
-
-# The next step is to go from one center + a radius to the coordinates of a circle that
-# is drawn by a multitude of straight lines.
-dat.gg <- circleLayoutVertices(packing, npoints=50)
-
-# Make the plot
-ggplot() + 
-  geom_polygon(data = dat.gg, aes(x, y, group = id, fill=as.factor(id)), colour = "white") +
-  geom_text(data = data_circle, size=5.75, aes(x, y, label = n), 
-            colour='white', fontface='bold') +
-  # geom_text(data = data_circle, size=5.75, aes(x, y-0.35, label = paste('(',V1, ')', sep='')), 
-  #           colour='white', fontface='bold') +
-  scale_size_continuous(range = c(1,2)) +
-  scale_fill_brewer(palette='RdBu',
-                    labels=c("2015 measure that was not reported electronically",
-                             "Measure modified since 2015 PoM",
-                             "Measure new in 2021 PoM",
-                             "Measure same as in 2015 PoM")) +
-  theme_void() + 
-  labs(fill="") +
-  theme(legend.position="top") +
-  coord_equal()
-ggsave('POM_charts/pom_update_type.jpg')
+# data_circle = 
+#   heatmap_data %>%
+#     group_by(UpdateType) %>%
+#     summarise(n = n_distinct(MeasureCode))
+# packing = circleProgressiveLayout(data_circle$n, sizetype='area')
+# # We can add these packing information to the initial data frame
+# data_circle <- cbind(data_circle, packing)
+# 
+# # The next step is to go from one center + a radius to the coordinates of a circle that
+# # is drawn by a multitude of straight lines.
+# dat.gg <- circleLayoutVertices(packing, npoints=50)
+# 
+# # Make the plot
+# ggplot() + 
+#   geom_polygon(data = dat.gg, aes(x, y, group = id, fill=as.factor(id)), colour = "white") +
+#   geom_text(data = data_circle, size=5.75, aes(x, y, label = n), 
+#             colour='white', fontface='bold') +
+#   # geom_text(data = data_circle, size=5.75, aes(x, y-0.35, label = paste('(',V1, ')', sep='')), 
+#   #           colour='white', fontface='bold') +
+#   scale_size_continuous(range = c(1,2)) +
+#   scale_fill_brewer(palette='RdBu',
+#                     labels=c("2015 measure that was not reported electronically",
+#                              "Measure modified since 2015 PoM",
+#                              "Measure new in 2021 PoM",
+#                              "Measure same as in 2015 PoM")) +
+#   theme_void() + 
+#   labs(fill="") +
+#   theme(legend.position="top") +
+#   coord_equal()
+# ggsave('POM_charts/12. Measure Update Type_v3.jpg')
 
 #### WAFFLE CHART ####
 waffle_data = 
   pom_report_net_data %>%
-  mutate(DPSiR = ifelse(is.na(DPSiR), 'None', DPSiR)) %>%
-  group_by(DPSiR) %>%
+  mutate(DAPSIR = ifelse(is.na(DAPSIR), 'None', DAPSIR)) %>%
+  group_by(DAPSIR) %>%
   summarise(n = n_distinct(MeasureCode)) %>%
   mutate(prop = (n/sum(n) * 100)) %>%
-  mutate(pct_text = paste(DPSiR, ' (', as.character(round(prop, 0)), '%)',
+  mutate(pct_text = paste(DAPSIR, ' (', as.character(round(prop, 0)), '%)',
                           sep=''))
 
-waffle_vector = round(waffle_data$prop/2, 0)
+waffle_vector = round(round(waffle_data$prop, 0)/2.5)
 names(waffle_vector) = waffle_data$pct_text
 
 waffle(waffle_vector, rows = 5, legend_pos='top',
@@ -279,13 +299,13 @@ waffle(waffle_vector, rows = 5, legend_pos='top',
                 '#0B5563'
        )
 )
-ggsave('POM_charts/pom_dpsir_waffle.jpg')
+ggsave('POM_charts/pom_DAPSIR_waffle.jpg')
 
 #### TREEMAP - Measure Type ####
 heatmap_data %>%
-  group_by(`Type of measure`) %>%
+  group_by(MeasureType) %>%
   summarise(n = n_distinct(MeasureCode)) %>%
-  mutate(prop = paste(`Type of measure`, '\n',
+  mutate(prop = paste(MeasureType, '\n',
                       '(', as.character(round(n/sum(n) * 100, 0)), '%)',
                       sep='')
   ) %>%
@@ -308,60 +328,60 @@ heatmap_data %>%
 ggsave('POM_charts/measure_type_percentage.jpg')
 
 #### BARCHART - Relevant KTMs ####
-heatmap_data %>%
-  group_by(RelevantKTMs) %>%
-  summarise(n = n_distinct(MeasureCode)) %>%
-  mutate(group = case_when(grepl("MSFD", RelevantKTMs) ~ "MSFD",
-                           grepl("WFD", RelevantKTMs) ~"WFD")) %>%
-  ggplot(aes(x=RelevantKTMs,
-             y=n,
-             fill=group)) + 
-  geom_bar(stat="identity") +
-  labs(title="Number of measures associated with Relevant KTMs",
-       y = '# of measures',
-       fill='')+
-  coord_flip() +
-  scale_y_continuous(expand = c(0, 0), limits=c(0, 65)) +
-  scale_x_discrete(limits=rev(sort(unique(heatmap_data$RelevantKTMs)))) +
-  scale_fill_manual(values=c('#145C9E',
-                             '#00A6A6',
-                             'grey50')
-  ) +
-  geom_text(aes(x=RelevantKTMs, y=n-0.5, label=n),
-            fontface='bold',
-            colour='white') +
-  theme_minimal() +
-  theme(legend.position='top',
-        plot.title=element_text(hjust=0.5))
-ggsave('POM_charts/relevant_ktms_bar.jpg')
+# heatmap_data %>%
+#   group_by(RelevantKTMs) %>%
+#   summarise(n = n_distinct(MeasureCode)) %>%
+#   mutate(group = case_when(grepl("MSFD", RelevantKTMs) ~ "MSFD",
+#                            grepl("WFD", RelevantKTMs) ~"WFD")) %>%
+#   ggplot(aes(x=RelevantKTMs,
+#              y=n,
+#              fill=group)) + 
+#   geom_bar(stat="identity") +
+#   labs(title="Number of measures associated with Relevant KTMs",
+#        y = '# of measures',
+#        fill='')+
+#   coord_flip() +
+#   scale_y_continuous(expand = c(0, 0), limits=c(0, 67)) +
+#   scale_x_discrete(limits=rev(sort(unique(heatmap_data$RelevantKTMs)))) +
+#   scale_fill_manual(values=c('#145C9E',
+#                              '#00A6A6',
+#                              'grey50')
+#   ) +
+#   geom_text(aes(x=RelevantKTMs, y=n-0.5, label=n),
+#             fontface='bold',
+#             colour='white') +
+#   theme_minimal() +
+#   theme(legend.position='top',
+#         plot.title=element_text(hjust=0.5))
+# ggsave('POM_charts/KTMGraph_v3.jpg')
 
 #### Heatmap of GES vs Measure Purpose ####
-heatmap_data %>%
-  group_by(MeasurePurpose, GEScomponent) %>%
-  summarise(n = n_distinct(MeasureCode)) %>%
-  ggplot() +
-  geom_raster(aes(x=GEScomponent, 
-                  y=MeasurePurpose, 
-                  fill=n),
-              hjust=0,
-              vjust=0.01) +
-  scale_x_discrete(position = "top", expand = c(0, 0), 
-                   limits=rev(levels(heatmap_data$GEScomponent))) +
-  scale_y_discrete(expand = c(0, 0), 
-                   limits=rev(sort(unique(heatmap_data$MeasurePurpose)))) +
-  scale_fill_viridis_c(option = "magma", direction=-1) +
-  # scale_color_gradient(low = "blue", high = "red") +
-  labs(fill='# of measures') +
-  theme_minimal() +
-  theme(legend.position = 'top',
-        axis.ticks.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.text.x = element_text(angle = 90),
-        axis.text.y = element_text(vjust=1.8),
-        panel.grid.major = element_line(colour = 'darkgrey'),
-        panel.border = element_rect(colour = 'darkgrey', fill=NA),
-        panel.ontop = TRUE)
-ggsave('POM_charts/pom_measure_purpose_vs_ges.jpg')
+# heatmap_data %>%
+#   group_by(MeasurePurpose, GEScomponent) %>%
+#   summarise(n = n_distinct(MeasureCode)) %>%
+#   ggplot() +
+#   geom_raster(aes(x=GEScomponent, 
+#                   y=MeasurePurpose, 
+#                   fill=n),
+#               hjust=0,
+#               vjust=0.01) +
+#   scale_x_discrete(position = "top", expand = c(0, 0), 
+#                    limits=rev(levels(heatmap_data$GEScomponent))) +
+#   scale_y_discrete(expand = c(0, 0), 
+#                    limits=rev(sort(unique(heatmap_data$MeasurePurpose)))) +
+#   scale_fill_viridis_c(option = "magma", direction=-1) +
+#   # scale_color_gradient(low = "blue", high = "red") +
+#   labs(fill='Number of Measures') +
+#   theme_minimal() +
+#   theme(legend.position = 'top',
+#         axis.ticks.y = element_blank(),
+#         axis.ticks.x = element_blank(),
+#         axis.text.x = element_text(angle = 90),
+#         axis.text.y = element_text(vjust=1.8),
+#         panel.grid.major = element_line(colour = 'darkgrey'),
+#         panel.border = element_rect(colour = 'darkgrey', fill=NA),
+#         panel.ontop = TRUE)
+# ggsave('POM_charts/pom_measure_purpose_vs_ges.jpg')
 
 #### BUBBLE - Feature vs Descriptor ####
 heatmap_data %>%
@@ -392,36 +412,36 @@ heatmap_data %>%
                    limits = rev(levels(heatmap_data$GEScomponent))) +
   theme_minimal() +
   guides(fill="none")
-ggsave('POM_charts/descriptor_feature_bubble.jpg')
+ggsave('POM_charts/9. Features Bubble Chart_v3.jpg')
 
 #### National Policies ####
-heatmap_data %>%
-  filter(!is.na(PolicyNational)) %>%
-  group_by(PolicyNational, GEScomponent) %>%
-  summarise(n = n_distinct(MeasureCode)) %>%
-  ggballoonplot(y='PolicyNational', x='GEScomponent', 
-                size='n',
-                fill='GEScomponent') +
-  labs(size='# of measures',
-       x='Descriptor',
-       y='Feature') +
-  scale_fill_manual(values=rev(c('#12AD5E', # biodiversity
-                                 '#CECD3C', # NI Species
-                                 '#39BFBE', # commercial shellfish
-                                 '#61B5E5', # food webs
-                                 '#70923D', # eutrophication
-                                 '#f78308',#rgb(0.9648438, 0.5195312, 0.3164062),#, # seafloor integrity
-                                 '#585995', # hydrographical conditions
-                                 '#5b5e5d', # contaminants
-                                 '#c21f42', # contaminants in seafood
-                                 '#176552', # marine litter
-                                 '#1A78B6' # underwater noise
-  )))  +
-  scale_size_continuous(breaks=c(2,5,10),
-                        range=c(1,10)) +
-  scale_x_discrete(position='top',
-                   limits = rev(levels(heatmap_data$GEScomponent))) +
-  scale_y_discrete(limits = rev(sort(unique(heatmap_data$PolicyNational)))) +
-  theme_minimal() +
-  guides(fill="none")
-ggsave('POM_charts/national_policies_bubble.jpg')
+# heatmap_data %>%
+#   filter(!is.na(PolicyNational)) %>%
+#   group_by(PolicyNational, GEScomponent) %>%
+#   summarise(n = n_distinct(MeasureCode)) %>%
+#   ggballoonplot(y='PolicyNational', x='GEScomponent', 
+#                 size='n',
+#                 fill='GEScomponent') +
+#   labs(size='# of measures',
+#        x='Descriptor',
+#        y='Feature') +
+#   scale_fill_manual(values=rev(c('#12AD5E', # biodiversity
+#                                  '#CECD3C', # NI Species
+#                                  '#39BFBE', # commercial shellfish
+#                                  '#61B5E5', # food webs
+#                                  '#70923D', # eutrophication
+#                                  '#f78308',#rgb(0.9648438, 0.5195312, 0.3164062),#, # seafloor integrity
+#                                  '#585995', # hydrographical conditions
+#                                  '#5b5e5d', # contaminants
+#                                  '#c21f42', # contaminants in seafood
+#                                  '#176552', # marine litter
+#                                  '#1A78B6' # underwater noise
+#   )))  +
+#   scale_size_continuous(breaks=c(2,5,10),
+#                         range=c(1,10)) +
+#   scale_x_discrete(position='top',
+#                    limits = rev(levels(heatmap_data$GEScomponent))) +
+#   scale_y_discrete(limits = rev(sort(unique(heatmap_data$PolicyNational)))) +
+#   theme_minimal() +
+#   guides(fill="none")
+# ggsave('POM_charts/national_policies_bubble.jpg')
